dab188dd4e4a6262b3bed1bdbd77fc82b97f769b
Galang Sakti G <wadepakmen97@gmail.com>
2024-03-15 11:00:14 +0700

    membuat input function

--- InsertionSortAlgoritma/InsertionSortAlgoritma.cpp
+++ InsertionSortAlgoritma/InsertionSortAlgoritma.cpp
@@ -1,18 +1,32 @@
 
 #include <iostream>
 using namespace std;
-
-void insertionSort() {
-    int i, n, temp;
-    int arr[11];
+int n;
+int arr[11];
 
 
+void input() {
+	while (true) {
+		cout << "Masukan banyak elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
 
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
 
-int main()
-{
-    
-}
+
+
 
 
